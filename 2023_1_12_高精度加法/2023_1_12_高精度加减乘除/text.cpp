1d8992c6c42b6fcf5d4ae71c083ed168235c67e3
乔辰 <674166088@qq.com>
2023-02-08 21:53:42 +0800

    算法

--- 2023_1_12_高精度加法/2023_1_12_高精度加减乘除/text.cpp
+++ 2023_1_12_高精度加法/2023_1_12_高精度加减乘除/text.cpp
@@ -3,43 +3,100 @@
 #include<vector>
 using namespace std;
 
-const int N = (int)1e6 + 10;
+//const int N = (int)1e6 + 10;
+//
+//vector<int>add(vector<int>& A, vector<int>& B)
+//{
+//	vector<int>C;
+//	int t = 0;	//存放进位信息
+//	for (int i = 0; i < A.size() || i < B.size(); i++)
+//	{
+//		if (i < A.size())
+//			t += A[i];
+//		if (i < B.size())
+//			t += B[i];
+//		C.push_back(t % 10);
+//		t /= 10;
+//	}
+//	if (t)
+//		C.push_back(1);
+//	return C;
+//}
+//int main()
+//{
+//	string a, b;
+//	vector<int>A, B, C;
+//
+//	cin >> a >> b;
+//	for (int i = (int)a.size() - 1; i >= 0; i--)
+//	{
+//		A.push_back(a[i] - '0');
+//	}
+//	for (int i = (int)b.size() - 1; i >= 0; i--)
+//	{
+//		B.push_back(b[i] - '0');
+//	}
+//	C = add(A, B);
+//	for (int i = (int)C.size() - 1; i >= 0; i--)
+//	{
+//		cout << C[i];
+//	}
+//	return 0;
+//}
+
+//压位操作
+
+const int N = (int)1e9;
 
 vector<int>add(vector<int>& A, vector<int>& B)
 {
 	vector<int>C;
-	int t = 0;	//存放进位信息
+	int t = 0;
 	for (int i = 0; i < A.size() || i < B.size(); i++)
 	{
-		if (i < A.size())
-			t += A[i];
-		if (i < B.size())
-			t += B[i];
-		C.push_back(t % 10);
-		t /= 10;
+		if (i < A.size()) t += A[i];
+		if (i < B.size()) t += B[i];
+		C.push_back(t % N);
+		t /= N;
 	}
-	if (t)
-		C.push_back(1);
+	if (t) C.push_back(t);
 	return C;
 }
+
 int main()
 {
 	string a, b;
-	vector<int>A, B, C;
-
+	vector<int>A, B;
 	cin >> a >> b;
-	for (int i = (int)a.size() - 1; i >= 0; i--)
+	for (int i = (int)a.size() - 1, j = 0, s = 0, t = 1; i >= 0; i--)
 	{
-		A.push_back(a[i] - '0');
+		s += (a[i] - '0') * t;
+		j++;
+		t *= 10;
+		if (j == 9 || i == 0)
+		{
+			A.push_back(s);
+			t = 1;
+			j = s = 0;
+		}
 	}
-	for (int i = (int)b.size() - 1; i >= 0; i--)
+	for (int i = (int)b.size() - 1, j = 0, s = 0, t = 1; i >= 0; i--)
 	{
-		B.push_back(b[i] - '0');
+		s += (b[i] - '0') * t;
+		j++;
+		t *= 10;
+		if (j == 9 || i == 0)
+		{
+			B.push_back(s);
+			t = 1;
+			j = s = 0;
+		}
 	}
-	C = add(A, B);
-	for (int i = (int)C.size() - 1; i >= 0; i--)
-	{
-		cout << C[i];
-	}
+	auto C = add(A, B);
+
+	cout << C.back() << endl;
+
+	for (int i = (int)C.size() - 2; i >= 0; i--)
+		printf("%09d", C[i]);
 	return 0;
 }
--- 2023_2_7_计数类DP/2023_2_7_计数类DP/text.cpp
+++ 2023_2_7_计数类DP/2023_2_7_计数类DP/text.cpp
@@ -3,3 +3,72 @@
 using namespace std;
 
 //整数划分
+
+//const int N = 1010, mod = 1e9 + 7;
+//int n, f[N];
+//
+//int main()
+//{
+//	cin >> n;
+//	f[0] = 1;
+//
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = i; j <= n; j++)
+//		{
+//			f[j] = f[j] + f[j - i];
+//		}
+//	}
+//	cout << f[n] << endl;
+//	return 0;
+//}
+
+//const int N = 1010, mod = 1e9 + 7;
+//int n, f[N][N];
+//
+//int main()
+//{
+//	cin >> n;
+//	for (int i = 0; i <= n; i++) f[i][0] = 1;
+//	
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = 1; j <= n; j++)
+//		{
+//			//cout << i << " " << j - i << " " << f[i][j - i] << "|";
+//			if (j - i >= 0)
+//			{
+//				f[i][j] = f[i - 1][j] + f[i][j - i];
+//			}
+//			else f[i][j] = f[i - 1][j];
+//		}
+//	}
+//	cout << f[n][n] << endl;
+//	//cout << f[4][1] << endl;
+//	return 0;
+//}
+
+//其他算法
+
+const int N = 1010;
+int n;
+int f[N][N];
+
+int main()
+{
+	cin >> n;
+	f[0][0] = 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			f[i][j] = f[i - 1][j - 1] + f[i - j][j];
+		}
+	}
+	int res = 0;
+	for (int i = 1; i <= n; i++) res += f[n][i];
+	cout << res << endl;
+	return 0;
+
+}
